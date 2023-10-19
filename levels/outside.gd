1b8af2b245c1c6220031b54fa9a03f41fa67972e
Daniil Chistyakov <ddani4929@gmail.com>
2023-10-19 23:13:29 +0300

    made smooth level transitions

--- levels/outside.gd
+++ levels/outside.gd
@@ -3,7 +3,6 @@ extends LevelParent
 #var packed_level: PackedScene = preload("res://levels/inside.tscn")
 
 func _on_gate_player_entered_inside(_body):
-#	var tween = get_tree().create_tween()
-#	tween.tween_property($Player, "player_speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://levels/inside.tscn")
-#	get_tree().change_scene_to_packed(packed_level)
+	var tween = create_tween()
+	tween.tween_property($Player, "player_speed", 0, 0.5)
+	$LevelTransition.change_scene("res://levels/inside.tscn")
