fbb0c1c212c3b32cb01e4c370899ce7fd4eae1a1
Daniil Chistyakov <ddani4929@gmail.com>
2023-10-30 21:46:24 +0300

    Made a few fixes

--- levels/level.gd
+++ levels/level.gd
@@ -32,14 +32,3 @@ func _on_player_grenade(pos, dir):
 	grenade.linear_velocity = dir * grenade.grenade_velocity
 	$Projectiles.add_child(grenade)
 
-
-func _on_garage_player_entered():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.45, 0.45), 1.0)
-	
-
-
-func _on_garage_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.3, 0.3), 1.0)
-
--- levels/outside.gd
+++ levels/outside.gd
@@ -6,3 +6,13 @@ func _on_gate_player_entered_inside(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "player_speed", 0, 0.5)
 	$LevelTransition.change_scene("res://levels/inside.tscn")
+
+
+func _on_garage_player_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.45, 0.45), 1)
+
+
+func _on_garage_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.3, 0.3), 2)
